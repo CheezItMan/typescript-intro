f700632144afa2a457beae0dddc51ae26bace75a
CheezItMan <chris@adadevelopersacademy.org>
2019-10-25 14:44:41 -0700

    refactor logTodo

--- index.ts
+++ index.ts
@@ -2,11 +2,25 @@
 
 import axios from 'axios';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 const URL = 'http://jsonplaceholder.typicode.com/todos/1';
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`Todo: ${id}
+  Title: ${title}
+  is ${ completed ? 'Complete': 'incomplete'}`);
+}
 
 axios.get(URL)
   .then(response => {
-    console.log(response.data);
+    const todo = response.data;
+    const {id, title, completed} = todo as Todo;
+    logTodo(id, title, completed);
+
   });
 
-  
